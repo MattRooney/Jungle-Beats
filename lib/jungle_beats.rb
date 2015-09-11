87e3f501a25b8c96d29910df113de31e60215f83
Matt Rooney <matthewjrooney@gmail.com>
2015-09-11 09:00:58 -0600

    Sixth commit. Fixed some bugs found in pry session, including append, play and all methods.

--- lib/jungle_beats.rb
+++ lib/jungle_beats.rb
@@ -19,15 +19,12 @@ class JungleBeats
     @beats
   end
 
-  def play
-    `say -r 500 -v Boing "#{beats}"`
-  end
-
   def append(beats)
     split_beats = beats.split
     split_beats[0..-1].each do |beat|
       linked_list.append(beat)
     end
+    self.all
   end
 
   def prepend(beats)
@@ -60,16 +57,23 @@ class JungleBeats
   def insert(data, num)
     linked_list.insert(data, num)
   end
+
+  def play
+    `say -r 500 -v Boing "#{linked_list.all}"`
+    self.count
+  end
+#require a pry session
 # binding.pry
 end
 
-# run_from_file =  ($PROGRAM_NAME == __FILE__)
-#
-# if run_from_file
-#   input_file = ARGV[0]
-#   beats_string = File.read(input_file)
-#   sounds_count = beats_string.split.length
-#
-#   JungleBeats.new(beats_string).play
-#   puts "Played #{sounds_count} sounds from `#{input_file}`"
-# end
+#running from the command line with a beats.txt doc
+run_from_file =  ($PROGRAM_NAME == __FILE__)
+
+if run_from_file
+  input_file = ARGV[0]
+  beats = File.read(input_file)
+  sounds_count = beats.split.length
+
+  JungleBeats.new(beats).play
+  puts "Played #{sounds_count} sounds from `#{input_file}`"
+end
